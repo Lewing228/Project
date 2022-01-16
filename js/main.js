049aed09db07cdf3e199cb9bf9293b8f90391765
Danil <rumyancev_danil@mail.ru>
2022-01-17 00:27:17 +0600

    Добавил модальное окно регистрации и маленькие изменения код в css

--- js/main.js
+++ js/main.js
@@ -135,3 +135,28 @@ function showTarget(targets, targetId) {
 
 main()
 
+
+// Модальное окно авторизации
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("btn_end_page");
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+  modal.style.display = "block";
+  document.body.style.overflow = "hidden";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+  document.body.style.overflow = "visible";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+	document.body.style.overflow = "visible";
+  }
+}
+
